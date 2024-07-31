ccb809fdfbd5d9cf86b4052ba4359bcde0ffac4e
awettedla <awettedla@gmail.com>
2024-07-31 00:28:03 -0400

    look

---
+++ src/main/java/runner/fast.java
@@ -0,0 +1,7 @@
+package runner;
+
+public class fast {
+    public static void main(String[] args) {
+        System.out.println("whos is the fastest runner?");
+    }
+}
--- src/main/java/runner/quikrunner.java
+++ src/main/java/runner/quikrunner.java
@@ -4,5 +4,7 @@ public class quikrunner {
     public static void main(String[] args) {
 
         System.out.println("good runner");
+
+        System.out.println("hiii");
     }
 }
