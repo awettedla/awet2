b65a76261317f8617ec3807dd5cc8c6311891941
awettedla <awettedla@gmail.com>
2024-07-31 01:01:44 -0400

    great

--- src/main/java/runner/fast.java
+++ src/main/java/runner/fast.java
@@ -2,6 +2,6 @@ package runner;
 
 public class fast {
     public static void main(String[] args) {
-        System.out.println("whos is the fastest runner?");
+        System.out.println("who is the fastest runner?");
     }
 }
